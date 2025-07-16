3fd89640967049c29c0fe0b4ed1120ec411b3dfb
trishacapitle <140716826+trishacapitle@users.noreply.github.com>
2025-07-16 19:51:57 +0800

    feat: add hover effect on navlinks

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,20 +5,50 @@ const Navbar = () => {
       <h1 className="font-secondary items-center text-3xl tracking-wide uppercase">
         Shop.co
       </h1>
-      <ul className="menu flex gap-6">
-        <li className="flex-center">
-          Shop{" "}
-          <span>
-            <BiChevronDown size={20} />
-          </span>
+      <ul className="menu flex gap-6 text-black font-primary">
+        <li>
+          <a
+            href="#"
+            className="flex-center transition-colors duration-300 ease-in-out hover:text-black/60"
+          >
+            Shop{" "}
+            <span>
+              <BiChevronDown size={20} />
+            </span>
+          </a>
         </li>
-        <li>On Sale</li>
-        <li>New Arrivals</li>
-        <li>Brands</li>
+        <li>
+          <a
+            href="#"
+            className="flex-center transition duration-300 ease-in-out hover:text-black/60"
+          >
+            On Sale
+          </a>
+        </li>
+        <li>
+          <a
+            href="#"
+            className="flex-center transition duration-300 ease-in-out hover:text-black/60"
+          >
+            New Arrivals
+          </a>
+        </li>
+        <li>
+          <a
+            href="#"
+            className="flex-center transition duration-300 ease-in-out hover:text-black/60"
+          >
+            Brands
+          </a>
+        </li>
       </ul>
       <div className="search-bar flex h-12 grow items-center gap-3 rounded-full bg-(--secondary-color) p-4">
         <BiSearch size={20} />
-        <input type="text" placeholder=" Search for products..." />
+        <input
+          type="text"
+          placeholder=" Search for products..."
+          className="flex-grow bg-transparent outline-none"
+        />
       </div>
       <div className="flex gap-3.5">
         <BiCart size={20} />
