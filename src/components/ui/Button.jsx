7b00411b2fcf4f24a31328eabcc3fcb13da305c6
trishacapitle <140716826+trishacapitle@users.noreply.github.com>
2025-07-17 01:11:38 +0800

    feat: change h prop than hard-code

--- src/components/ui/Button.jsx
+++ src/components/ui/Button.jsx
@@ -1,7 +1,7 @@
 const Button = ({ title, className = ""}) => {
   return (
     <button
-      className={`font-primary h-14 rounded-full w-fit ${className} transition-all duration-300 ease-in-out hover:scale-105`}
+      className={`font-primary rounded-full w-fit ${className} transition-all duration-300 ease-in-out hover:scale-105`}
     >
       {title}
     </button>
