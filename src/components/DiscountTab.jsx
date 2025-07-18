ff15aa9199c097640e6b44c3ecdf35b178e81915
trishacapitle <140716826+trishacapitle@users.noreply.github.com>
2025-07-19 00:42:11 +0800

    feat: close button functionality

--- src/components/DiscountTab.jsx
+++ src/components/DiscountTab.jsx
@@ -1,14 +1,26 @@
 import { BiX } from "react-icons/bi";
+import { useState } from "react";
+
 const DiscountTab = () => {
+const [isOpen, setIsOpen] = useState(true);
+
+  const handleClose = () => {
+    setIsOpen(false);
+  };
+
   return (
-    <div className="flex-center font-primary bg-black h-9 text-sm text-white">
-      <p>
-        Sign up and get 20% off to your first order.{" "}
-        <a href="#" className="underline">
-          Sign Up Now
-        </a>
-      </p>
-      <BiX size={20} className="absolute right-10"/>
+    <div>
+      {isOpen && (
+        <div className="discount-tab flex-center font-primary h-9 bg-black text-sm text-white">
+          <p>
+            Sign up and get 20% off to your first order.{" "}
+            <a href="#" className="underline">Sign Up Now</a>
+          </p>
+          <button className="absolute right-10" onClick={handleClose}>
+            <BiX size={20} />
+          </button>
+        </div>
+      )}
     </div>
   );
 };
