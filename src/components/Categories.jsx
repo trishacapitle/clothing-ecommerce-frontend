0533834f0e646a811d2a7f09bb7eaf2726805e38
trishacapitle <140716826+trishacapitle@users.noreply.github.com>
2025-07-22 06:25:59 +0800

    feat: configure styling for mobile screens

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -1,10 +1,10 @@
 const Categories = () => {
   return (
-    <section className="col-center rounded-4xl bg-[#F0F0F0] p-16 w-fit m-auto">
-      <h1 className="font-secondary mb-16 text-5xl uppercase">
+    <section className="col-center m-auto w-fit rounded-4xl bg-[#F0F0F0] p-8 sm:p-16">
+      <h1 className="font-secondary mb-8 text-4xl uppercase sm:mb-16 sm:text-5xl">
         browse by dress style
       </h1>
-      <div className="flex-center font-primary gap-5">
+      <div className="flex-center sm:flex-row flex-col font-primary gap-5">
         <div className="casual relative">
           <p className="absolute top-6 left-9 text-2xl font-bold">Casual</p>
           <img
@@ -18,17 +18,17 @@ const Categories = () => {
           <img
             src="/formal.png"
             alt="Formal Wear"
-            className="h-72 w-2xl rounded-2xl transition-shadow duration-300 ease-in-out hover:shadow-xl"
+            className="h-72 w-2xl rounded-2xl object-cover transition-shadow duration-300 ease-in-out hover:shadow-xl"
           />
         </div>
       </div>
-      <div className="flex-center font-primary mt-5 gap-5">
+      <div className="flex-center sm:flex-row flex-col font-primary mt-5 gap-5">
         <div className="party relative">
           <p className="absolute top-6 left-9 text-2xl font-bold">Party</p>
           <img
             src="/party.png"
             alt="Party Wear"
-            className="h-72 w-2xl rounded-2xl transition-shadow duration-300 ease-in-out hover:shadow-xl"
+            className="h-72 w-2xl rounded-2xl object-cover transition-shadow duration-300 ease-in-out hover:shadow-xl"
           />
         </div>
         <div className="gym relative">
