7b5a85f6346a1276f238da8fb5bffec32c664a60
trishacapitle <140716826+trishacapitle@users.noreply.github.com>
2025-07-22 05:29:38 +0800

    feat: configure gaps, layout, and spacing for mobile screens

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,40 +1,50 @@
 import bg from "/landing_page.png";
+import bgMobile from "/landing_page_mobile.png";
 import vector from "/vector.png";
 import Button from "./ui/Button";
 
 const Hero = () => {
   return (
-    <section className="flex-center relative h-[660px] w-full bg-[#F2F0F1]">
-      <img src={bg} alt="hero image" className="absolute" />
-      <div className="content flex-center z-99 h-full w-[80dvw] gap-24">
-        <div className="flex flex-1/2 flex-col gap-8">
-          <h1 className="font-secondary sm:text-6xl text-4xl uppercase">
+    <section className="sm:flex-center col-center relative w-full bg-[#F2F0F1] sm:h-[660px]">
+      <img src={bg} alt="hero image" className="absolute hidden sm:flex" />
+      <div className="content flex-center z-99 h-full gap-24 px-4 sm:w-[80dvw] sm:px-0">
+        <div className="mt-10 flex flex-1/2 flex-col gap-5 sm:mt-0 sm:gap-8">
+          <h1 className="font-secondary text-4xl uppercase sm:text-6xl">
             find clothes that matches your style
           </h1>
-          <p className="font-primary text-black/60 text-sm sm:text-base">
+          <p className="font-primary text-sm text-black/60 sm:text-base">
             Browse through our diverse range of meticulously crafted garments,
             designed to bring out your individuality and cater to your sense of
             style.
           </p>
-          <Button title="Shop Now" className="h-14 bg-black px-20 text-white" />
+          <Button
+            title="Shop Now"
+            className="h-14 w-full grow bg-black px-20 text-white sm:w-fit"
+          />
           <div className="shop-performance font-primary flex-center justify-between">
             <div>
-              <h1 className="sm:text-4xl text-2xl font-bold">200+</h1>
-              <p className="text-xs sm:text-base text-black/60">International Brands</p>
+              <h1 className="text-2xl font-bold sm:text-4xl">200+</h1>
+              <p className="text-xs text-black/60 sm:text-base">
+                International Brands
+              </p>
             </div>
             <hr className="h-20 w-[1px] border-0 bg-black/10" />
             <div>
-              <h1 className="sm:text-4xl text-2xl font-bold">2000+</h1>
-              <p className="text-xs sm:text-base text-black/60">High-Quality Products</p>
+              <h1 className="text-2xl font-bold sm:text-4xl">2000+</h1>
+              <p className="text-xs text-black/60 sm:text-base">
+                High-Quality Products
+              </p>
             </div>
             <hr className="h-20 w-[1px] border-0 bg-black/10" />
             <div>
-              <h1 className="sm:text-4xl text-2xl font-bold">30,000+</h1>
-              <p className="text-xs sm:text-base text-black/60">Happy Customers</p>
+              <h1 className="text-2xl font-bold sm:text-4xl">30,000+</h1>
+              <p className="text-xs text-black/60 sm:text-base">
+                Happy Customers
+              </p>
             </div>
           </div>
         </div>
-        <div className="relative h-full flex-1/2">
+        <div className="relative hidden h-full flex-1/2 sm:inline-block">
           <img
             src={vector}
             alt="vector image"
@@ -46,6 +56,13 @@ const Hero = () => {
             className="absolute top-10 right-10 animate-bounce"
           />
         </div>
+      </div>
+      <div className="w-full">
+        <img
+          src={bgMobile}
+          alt="landing page background image for mobile devices"
+          className="sm:hidden w-full"
+        />
       </div>
     </section>
   );
