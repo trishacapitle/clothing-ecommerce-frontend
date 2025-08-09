e97f2ff05af1ab7327fe92d8f64a7bf2987bfd95
trishacapitle <140716826+trishacapitle@users.noreply.github.com>
2025-08-10 02:10:08 +0800

    fix: correct code syntax and added more product review information

--- src/assets/data.js
+++ src/assets/data.js
@@ -1,102 +1,157 @@
 export const data = {
-  "products": {
-    "newArrivals": [
+  products: {
+    newArrivals: [
       {
-        "id": "product1",
-        "name": "T‑shirt with Tape Details",
-        "rating": 4.5,
-        "price": 120,
-        "image": "tshirt_with_tape_details.png"
+        id: "product1",
+        name: "T‑shirt with Tape Details",
+        rating: 4.5,
+        price: 120,
+        image: "tshirt_with_tape_details.png",
+        review: [
+          {
+            id: 1,
+            productRating: 4.5,
+            customerName: "Samantha D.",
+            productReview:
+              "I absolutely love this t-shirt! The design is unique and the fabric feels so comfortable. As a fellow designer, I appreciate the attention to detail. It's become my favorite go-to shirt.",
+            reviewDate: "August 14, 2023",
+          },
+          {
+            id: 2,
+            productRating: 4,
+            customerName: "Alex M.",
+            productReview:
+              "The t-shirt exceeded my expectations! The colors are vibrant and the print quality is top-notch. Being a UI/UX designer myself, I'm quite picky about aesthetics, and this t-shirt definitely gets a thumbs up from me.",
+            reviewDate: "August 15, 2023",
+          },
+          {
+            id: 3,
+            productRating: 3.5,
+            customerName: "Ethan R.",
+            productReview:
+              "This t-shirt is a must-have for anyone who appreciates good design. The minimalistic yet stylish pattern caught my eye, and the fit is perfect. I can see the designer's touch in every aspect of this shirt.",
+            reviewDate: "August 16, 2023",
+          },
+          {
+            id: 4,
+            productRating: 4,
+            customerName: "Olivia P.",
+            productReview:
+              "As a UI/UX enthusiast, I value simplicity and functionality. This t-shirt not only represents those principles but also feels great to wear. It's evident that the designer poured their creativity into making this t-shirt stand out.",
+            reviewDate: "August 17, 2023",
+          },
+          {
+            id: 1,
+            productRating: 4.5,
+            customerName: "Liam K.",
+            productReview:
+              "This t-shirt is a fusion of comfort and creativity. The fabric is soft, and the design speaks volumes about the designer's skill. It's like wearing a piece of art that reflects my passion for both design and fashion.",
+            reviewDate: "August 18, 2023",
+          },
+          {
+            id: 1,
+            productRating: 4.5,
+            customerName: "Ava H.",
+            productReview:
+              "I'm not just wearing a t-shirt; I'm wearing a piece of design philosophy. The intricate details and thoughtful layout of the design make this shirt a conversation starter.",
+            reviewDate: "August 19, 2023",
+          },
+        ],
       },
       {
-        "id": "product2",
-        "name": "Skinny Fit Jeans",
-        "rating": 3.5,
-        "price": 240,
-        "image": "skinny_fit_jeans.png",
-        "discount": true,
-        "originalPrice": 260,
+        id: "product2",
+        name: "Skinny Fit Jeans",
+        rating: 3.5,
+        price: 240,
+        image: "skinny_fit_jeans.png",
+        discount: true,
+        originalPrice: 260,
       },
       {
-        "id": "product3",
-        "name": "Checkered Shirt",
-        "rating": 4.5,
-        "price": 180,
-        "image": "checkered_shirt.png"
+        id: "product3",
+        name: "Checkered Shirt",
+        rating: 4.5,
+        price: 180,
+        image: "checkered_shirt.png",
       },
       {
-        "id": "product4",
-        "name": "Sleeve Striped T‑shirt",
-        "rating": 4.5,
-        "price": 130,
-        "image": "sleeve_striped_tshirt.png",
-        "discount": true,
-        "originalPrice": 160,
-      }
+        id: "product4",
+        name: "Sleeve Striped T‑shirt",
+        rating: 4.5,
+        price: 130,
+        image: "sleeve_striped_tshirt.png",
+        discount: true,
+        originalPrice: 160,
+      },
     ],
-    "topSelling": [
+    topSelling: [
       {
-        "id": "product1",
-        "name": "Vertical Striped Shirt",
-        "rating": 5.0,
-        "price": 212,
-        "image": "vertical_striped_shirt.png",
-        "discount": true,
-        "originalPrice": 232,
+        id: "product1",
+        name: "Vertical Striped Shirt",
+        rating: 5.0,
+        price: 212,
+        image: "vertical_striped_shirt.png",
+        discount: true,
+        originalPrice: 232,
       },
       {
-        "id": "product2",
-        "name": "Courage Graphic T‑shirt",
-        "rating": 4.0,
-        "price": 145,
-        "image": "graphic_tshirt.png"
+        id: "product2",
+        name: "Courage Graphic T‑shirt",
+        rating: 4.0,
+        price: 145,
+        image: "graphic_tshirt.png",
       },
       {
-        "id": "product3",
-        "name": "Loose Fit Bermuda Shorts",
-        "rating": 3.0,
-        "price": 80,
-        "image": "loose_fit_bermuda.png"
+        id: "product3",
+        name: "Loose Fit Bermuda Shorts",
+        rating: 3.0,
+        price: 80,
+        image: "loose_fit_bermuda.png",
       },
       {
-        "id": "product4",
-        "name": "Faded Skinny Jeans",
-        "rating": 4.5,
-        "price": 210,
-        "image": "faded_skinny_jeans.png"
-      }
-    ]
+        id: "product4",
+        name: "Faded Skinny Jeans",
+        rating: 4.5,
+        price: 210,
+        image: "faded_skinny_jeans.png",
+      },
+    ],
   },
-  "ratings": [
+  ratings: [
     {
-      "id": "customer1",
-      "stars": 5,
-      "name": "Sarah M.",
-      "review": "I'm blown away by the quality and style of the clothes I received from Shop.co. From casual wear to elegant dresses, every piece I've bought has exceeded my expectations."
+      id: "customer1",
+      stars: 5,
+      name: "Sarah M.",
+      review:
+        "I'm blown away by the quality and style of the clothes I received from Shop.co. From casual wear to elegant dresses, every piece I've bought has exceeded my expectations.",
     },
     {
-      "id": "customer2",
-      "stars": 5,
-      "name": "Alex K.",
-      "review": "Finding clothes that align with my personal style used to be a challenge until I discovered Shop.co. The range of options they offer is truly remarkable, catering to a variety of tastes and occasions."
+      id: "customer2",
+      stars: 5,
+      name: "Alex K.",
+      review:
+        "Finding clothes that align with my personal style used to be a challenge until I discovered Shop.co. The range of options they offer is truly remarkable, catering to a variety of tastes and occasions.",
     },
     {
-      "id": "customer3",
-      "stars": 5,
-      "name": "James L.",
-      "review": "As someone who's always on the lookout for unique fashion pieces, I'm thrilled to have stumbled upon Shop.co. The selection of clothes is not only diverse but also on-point with the latest trends."
+      id: "customer3",
+      stars: 5,
+      name: "James L.",
+      review:
+        "As someone who's always on the lookout for unique fashion pieces, I'm thrilled to have stumbled upon Shop.co. The selection of clothes is not only diverse but also on-point with the latest trends.",
     },
     {
-      "id": "customer4",
-      "stars": 5,
-      "name": "Marie S.",
-      "review": "I'm blown away by the quality and style of the clothes I received from Shop.co. From casual wear to elegant dresses, every piece I've bought has exceeded my expectations."
+      id: "customer4",
+      stars: 5,
+      name: "Marie S.",
+      review:
+        "I'm blown away by the quality and style of the clothes I received from Shop.co. From casual wear to elegant dresses, every piece I've bought has exceeded my expectations.",
     },
     {
-      "id": "customer5",
-      "stars": 5,
-      "name": "Alex C.",
-      "review": "As someone who's always on the lookout for unique fashion pieces, I'm thrilled to have stumbled upon Shop.co. The selection of clothes is not only diverse but also on-point with the latest trends."
-    }
-  ]
-}
+      id: "customer5",
+      stars: 5,
+      name: "Alex C.",
+      review:
+        "As someone who's always on the lookout for unique fashion pieces, I'm thrilled to have stumbled upon Shop.co. The selection of clothes is not only diverse but also on-point with the latest trends.",
+    },
+  ],
+};
