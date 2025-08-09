ac340383d80518b6406824a85f4640d80d414bbe
trishacapitle <140716826+trishacapitle@users.noreply.github.com>
2025-08-10 04:39:23 +0800

    fix: layout issue for hero section

--- src/pages/LandingPage/components/Hero.jsx
+++ src/pages/LandingPage/components/Hero.jsx
@@ -5,14 +5,14 @@ import Button from "../../../components/ui/Button";
 
 const Hero = () => {
   return (
-    <section className="sm:flex-center col-center relative w-full bg-[#F2F0F1] sm:h-[660px]">
+    <section className="sm:flex-center col-center relative w-full bg-[#F2F0F1] lg:h-[660px]">
       <img
         src={bg}
         alt="hero image"
-        className="absolute bottom-0 hidden sm:flex"
+        className="absolute bottom-0 hidden md:flex"
       />
       <div className="content flex-center z-50 h-full gap-24 px-4 sm:w-[80dvw] sm:px-0">
-        <div className="mt-10 flex flex-1/2 flex-col gap-5 sm:mt-0 sm:gap-8">
+        <div className="mt-10 flex flex-1/2 flex-col gap-5 sm:mt-0 lg:gap-8">
           <h1 className="font-secondary text-4xl uppercase sm:text-6xl">
             find clothes that matches your style
           </h1>
@@ -27,28 +27,28 @@ const Hero = () => {
           />
           <div className="shop-performance font-primary flex-center justify-between">
             <div>
-              <h1 className="text-2xl font-bold sm:text-4xl">200+</h1>
-              <p className="text-xs text-black/60 sm:text-base">
+              <h1 className="text-2xl font-bold md:text-4xl">200+</h1>
+              <p className="text-xs text-black/60 md:text-base">
                 International Brands
               </p>
             </div>
             <hr className="h-20 w-[1px] border-0 bg-black/10" />
             <div>
-              <h1 className="text-2xl font-bold sm:text-4xl">2000+</h1>
-              <p className="text-xs text-black/60 sm:text-base">
+              <h1 className="text-2xl font-bold md:text-4xl">2000+</h1>
+              <p className="text-xs text-black/60 md:text-base">
                 High-Quality Products
               </p>
             </div>
             <hr className="h-20 w-[1px] border-0 bg-black/10" />
             <div>
-              <h1 className="text-2xl font-bold sm:text-4xl">30,000+</h1>
-              <p className="text-xs text-black/60 sm:text-base">
+              <h1 className="text-2xl font-bold md:text-4xl">30,000+</h1>
+              <p className="text-xs text-black/60 md:text-base">
                 Happy Customers
               </p>
             </div>
           </div>
         </div>
-        <div className="relative hidden h-full flex-1/2 sm:inline-block">
+        <div className="relative hidden h-full flex-1/2 md:inline-block">
           <img
             src={vector}
             alt="vector image"
@@ -70,12 +70,12 @@ const Hero = () => {
         <img
           src={vector}
           alt="vector image"
-          className="absolute top-10 right-5 sm:hidden"
+          className="absolute top-10 right-5 md:hidden"
         />
         <img
           src={bgMobile}
           alt="landing page background image for mobile devices"
-          className="w-full sm:hidden"
+          className="w-full md:hidden"
         />
       </div>
     </section>
