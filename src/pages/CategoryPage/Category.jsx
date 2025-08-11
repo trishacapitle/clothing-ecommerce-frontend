83619670241241aa2079a7d4f786e56010e48282
trishacapitle <140716826+trishacapitle@users.noreply.github.com>
2025-08-12 02:14:13 +0800

    test: check code if URLparams is correctly passed

--- src/pages/CategoryPage/Category.jsx
+++ src/pages/CategoryPage/Category.jsx
@@ -1,9 +1,13 @@
-import React from 'react'
+import { useParams } from "react-router-dom";
 
 const Category = () => {
-  return (
-    <div>Category</div>
-  )
-}
+  const { type } = useParams();
 
-export default Category
+  return (
+    <div>
+      <h1>Category {type}</h1>
+    </div>
+  );
+};
+
+export default Category;
