6eeeea84e3e78b561b6c6a37660db9a4a8cb7092
trishacapitle <trishacapitle01@gmail.com>
2025-08-19 16:34:07 +0800

    fix: add conditional rendering for products with no ratings

--- src/components/ui/Card.jsx
+++ src/components/ui/Card.jsx
@@ -8,18 +8,20 @@ const Card = ({ product }) => {
   const stars = "★".repeat(filledStars) + "☆".repeat(emptyStars);
 
   return (
-    <div className="font-primary flex h-fit w-72 cursor-pointer flex-col gap-4 rounded-3xl border border-black/0 p-5 transition-all duration-300 ease-in-out hover:border-black/40 hover:shadow-lg hover:-translate-y-2 my-5">
+    <div className="font-primary my-5 flex h-fit w-72 cursor-pointer flex-col gap-4 rounded-3xl border border-black/0 p-5 transition-all duration-300 ease-in-out hover:-translate-y-2 hover:border-black/40 hover:shadow-lg">
       <div className="flex-center">
-        <img src={`/${image}`} alt={name} className="w-72 h-72 rounded-3xl" />
+        <img src={`/${image}`} alt={name} className="h-72 w-72 rounded-3xl" />
       </div>
 
       <div className="details flex flex-col gap-1">
         <h2 className="text-xl font-bold">{name}</h2>
 
-        <div className="flex items-center gap-3">
-          <span className="text-base text-[gold]">{stars}</span>
-          <span className="text-sm text-black/60">{rating}/5</span>
-        </div>
+        {rating && (
+          <div className="flex items-center gap-3">
+            <span className="text-base text-[gold]">{stars}</span>
+            <span className="text-sm text-black/60">{rating}/5</span>
+          </div>
+        )}
 
         <div className="flex gap-2.5">
           <h1 className="text-2xl font-bold">${price}</h1>
