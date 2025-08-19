d27555e65eb6d5b7dd3ea768c66dfd62979c80a6
trishacapitle <trishacapitle01@gmail.com>
2025-08-19 21:59:36 +0800

    feat: product showcase structure (no styling yet)

--- src/pages/ProductPage/components/ProductShowcase.jsx
+++ src/pages/ProductPage/components/ProductShowcase.jsx
@@ -1,9 +1,39 @@
-import React from 'react'
+import { useParams } from "react-router-dom";
+import { data } from "../../../assets/data";
+import Pagination from "../../CategoryPage/ui/Pagination";
+import StarRatings from "../../../components/ui/StarRatings";
+import Pricetag from "../../../components/ui/Pricetag";
 
 const ProductShowcase = () => {
-  return (
-    <div>ProductShowcase</div>
-  )
-}
+  const { id } = useParams();
+  console.log(id);
+  const { products } = data;
+  const selectedProduct = products.find((product) => product.id === Number(id));
 
-export default ProductShowcase
+  console.log(selectedProduct);
+
+  const { name, rating, image } =
+    selectedProduct;
+
+  return (
+    <section>
+      <Pagination />
+      <div>
+        <div className="product-picture">
+          <img src={`/${image}`} alt="product picture" />
+        </div>
+        <div className="product-details">
+          <h1>{name}</h1>
+          {rating ? (
+            <StarRatings rating={rating} />
+          ) : (
+            <span>No ratings yet</span>
+          )}
+          <Pricetag product={selectedProduct} />
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default ProductShowcase;
