29425915c9ebfa61972a4b0f57c2d0327dfa10a7
trishacapitle <trishacapitle01@gmail.com>
2025-08-20 23:08:38 +0800

    refactor: replace price with price tag component

--- src/components/ui/Card.jsx
+++ src/components/ui/Card.jsx
@@ -1,7 +1,7 @@
-const Card = ({ product }) => {
-  const { image, name, price, rating, discount, originalPrice } = product;
+import Pricetag from "./Pricetag";
 
-  const discountPercentage = ((originalPrice - price) / originalPrice) * 100;
+const Card = ({ product }) => {
+  const { image, name, rating} = product;
 
   const filledStars = Math.floor(rating);
   const emptyStars = 5 - filledStars;
@@ -23,19 +23,7 @@ const Card = ({ product }) => {
           </div>
         )}
 
-        <div className="flex gap-2.5">
-          <h1 className="text-2xl font-bold">${price}</h1>
-          {discount && (
-            <div className="flex-center gap-2.5">
-              <div className="text-2xl font-bold text-black/40 line-through">
-                ${originalPrice}
-              </div>
-              <div className="flex-center h-8 w-14 rounded-full bg-[#FF3333]/10 px-3 text-base font-medium text-[#FF3333]">
-                -{discountPercentage.toFixed(0)}%{" "}
-              </div>
-            </div>
-          )}
-        </div>
+        <Pricetag product={product} />
       </div>
     </div>
   );
