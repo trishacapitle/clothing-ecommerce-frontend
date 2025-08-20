8eba15a5273453c01de611dbf35c10fb166bab56
trishacapitle <trishacapitle01@gmail.com>
2025-08-20 23:17:43 +0800

    fix: remove wrong return

--- src/pages/CategoryPage/ui/Pagination.jsx
+++ src/pages/CategoryPage/ui/Pagination.jsx
@@ -1,9 +1,7 @@
 
 const Pagination = () => {
   return (
-    <div className="font-primary tracking-wide">
-      <span className="text-black/60">Home &gt; Shop &gt; Men &gt; </span> T-shirts
-    </div>
+    <></>
   );
 }
 
