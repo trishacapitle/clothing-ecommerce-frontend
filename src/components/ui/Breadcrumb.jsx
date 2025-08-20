9c209a40e2548d72109a4a47b3296c8f2b32bc47
trishacapitle <trishacapitle01@gmail.com>
2025-08-20 23:17:10 +0800

    fix: correct project file

---
+++ src/components/ui/Breadcrumb.jsx
@@ -0,0 +1,11 @@
+
+const Breadcrumb = () => {
+  return (
+    <div className="font-primary tracking-wide">
+      <span className="text-black/60">Home &gt; Shop &gt; Men &gt; </span>{" "}
+      T-shirts
+    </div>
+  );
+}
+
+export default Breadcrumb
