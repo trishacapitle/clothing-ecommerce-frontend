0ea2330151a64d563268c8221a3c3f6d679f05d4
trishacapitle <trishacapitle01@gmail.com>
2025-08-20 23:24:58 +0800

    feat: add margin top and padding x to breadcrumb for mobile layout

--- src/components/ui/Breadcrumb.jsx
+++ src/components/ui/Breadcrumb.jsx
@@ -1,7 +1,7 @@
 
 const Breadcrumb = () => {
   return (
-    <div className="font-primary tracking-wide">
+    <div className="font-primary tracking-wide mt-5 px-4 sm:p-0">
       <span className="text-black/60">Home &gt; Shop &gt; Men &gt; </span>{" "}
       T-shirts
     </div>
