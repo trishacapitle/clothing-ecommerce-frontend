8ac608503e83585ad70e0016995d7ad066c3a4d0
trishacapitle <trishacapitle01@gmail.com>
2025-08-20 23:18:24 +0800

    fix: correct component

--- src/pages/ProductPage/Product.jsx
+++ src/pages/ProductPage/Product.jsx
@@ -1,10 +1,12 @@
 import ProductShowcase from "./components/ProductShowcase";
 import ProductDetails from "./components/ProductDetails";
 import ProductReviews from "./components/ProductReviews";
+import Breadcrumb from "../../components/ui/Breadcrumb";
 
 const Product = () => {
   return (
     <div>
+      <Breadcrumb  />
       <ProductShowcase />
       <div>
         <ProductDetails />
