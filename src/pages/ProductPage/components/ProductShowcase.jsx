910dff9e4e2f75ad8e93f188b7b9f323001dfe52
trishacapitle <trishacapitle01@gmail.com>
2025-08-22 16:32:01 +0800

    feat: add styles to add to cart button fix: incorrect html tags

--- src/pages/ProductPage/components/ProductShowcase.jsx
+++ src/pages/ProductPage/components/ProductShowcase.jsx
@@ -1,11 +1,13 @@
 import { useParams } from "react-router-dom";
 import { useState } from "react";
+import {BiPlus, BiMinus} from "react-icons/bi"
 import { data } from "../../../assets/data";
 import Pricetag from "../../../components/ui/Pricetag";
 import StarRatings from "../../../components/ui/StarRatings";
 
 const ProductShowcase = () => {
-  const [ qty, setQty ] = useState(0);
+  const [qty, setQty] = useState(0);
+  const [isSelected, setisSelected] = useState(false);
 
   const { id } = useParams();
   const { products } = data;
@@ -22,6 +24,7 @@ const ProductShowcase = () => {
     const res = qty - 1;
     setQty(res);
   };
+  
 
   return (
     <section className="flex-center mt-4 w-full">
@@ -47,26 +50,58 @@ const ProductShowcase = () => {
             This graphic t-shirt which is perfect for any occasion. Crafted from
             a soft and breathable fabric, it offers superior comfort and style.
           </p>
-          <hr className="border-black/20 my-2" />
-          <div className="product-selector flex gap-2">
-            <div className="color-option active:border-2 active:border-black h-10 w-10 rounded-full border border-black/60 bg-amber-500"></div>
-            <div className="color-option active:border-2 active:border-black h-10 w-10 rounded-full border border-black/60 bg-green-800"></div>
-            <div className="color-option active:border-2 active:border-black h-10 w-10 rounded-full border border-black/60 bg-blue-900"></div>
+          <div>
+            <hr className="border-black/20" />
+            <div className="my-6">
+              <p>Select Colors</p>
+              <div className="product-selector mt-4 flex gap-2">
+                <button className="color-option h-10 w-10 rounded-full border border-black/60 bg-amber-500 active:border-2 active:border-black"></button>
+                <button className="color-option h-10 w-10 rounded-full border border-black/60 bg-green-800 active:border-2 active:border-black"></button>
+                <button className="color-option h-10 w-10 rounded-full border border-black/60 bg-blue-900 active:border-2 active:border-black"></button>
+              </div>
+            </div>
+            <hr className="border-black/20" />
+            <div className="my-6">
+              <p>Choose Size</p>
+
+              <div className="product-selector mt-4 flex gap-2">
+                <button
+                  className={`font-primary color-option rounded-full bg-[#F0F0F0] px-8 py-3 ${isSelected}`}
+                >
+                  Small
+                </button>
+                <button
+                  className={`font-primary color-option rounded-full bg-[#F0F0F0] px-8 py-3 ${isSelected}`}
+                >
+                  Medium
+                </button>
+                <button
+                  className={`font-primary color-option rounded-full ${isSelected} bg-[#F0F0F0] px-8 py-3 ${isSelected}`}
+                >
+                  Large
+                </button>
+              </div>
+            </div>
           </div>
-          <hr className="border-black/20 my-2" />
-          <div className="qty-selector flex w-32 items-center justify-between rounded-full bg-black/10 px-4 py-2">
-            <button
-              className="decrease px-2 text-xl font-bold"
-              onClick={handleDecrease}
-            >
-              -
-            </button>
-            <p>{qty}</p>
-            <button
-              className="increase px-2 text-xl font-bold"
-              onClick={handleIncrease}
-            >
-              +
+          <hr className="border-black/20" />
+          <div className="my-6 flex gap-3">
+            <div className="qty-selector flex w-36 items-center justify-between rounded-full bg-black/10 px-4 py-2">
+              <button
+                className="decrease p-2 text-xl font-bold"
+                onClick={handleDecrease}
+              >
+                <BiMinus />
+              </button>
+              <p>{qty}</p>
+              <button
+                className="increase p-2 text-xl font-bold"
+                onClick={handleIncrease}
+              >
+                <BiPlus />
+              </button>
+            </div>
+            <button className="grow font-primary add-to-cart qty-selector flex w-36 items-center justify-center rounded-full bg-black text-white">
+              Add to cart
             </button>
           </div>
         </div>
